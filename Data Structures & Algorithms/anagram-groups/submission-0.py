9bc678e541b49eeaba0d056ec066b5c91356ee18
Wiktoria Stachera <73538655+Wiki35@users.noreply.github.com>
2026-09-18 19:34:46 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,13 @@
+# time O(m * nlogn), space O(m * n)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+
+        for s in strs:
+            key = "".join(sorted(s))
+            if key not in seen:
+                seen[key] = []
+            seen[key].append(s)
+    
+        return list(seen.values())
